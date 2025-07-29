693722bbc0ad079fbfaafd8a00f96d763b1fc005
Shuming Zhang <chriszhangshu@gmail.com>
2025-07-29 01:57:49 -0400

    Added JDBC and Redis to Maven dependencies, added an API to manage user's favorite job items.

---
+++ src/main/java/com/example/jobrec/entity/ResultResponse.java
@@ -0,0 +1,11 @@
+package com.example.jobrec.entity;
+
+public class ResultResponse {
+    public String result;
+    public ResultResponse(){
+
+    }
+    public ResultResponse(String result){
+        this.result = result;
+    }
+}
